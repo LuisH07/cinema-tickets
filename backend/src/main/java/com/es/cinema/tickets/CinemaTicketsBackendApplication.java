1ce934cdf43875db33c37766ede9db063a2663dd
Luis <luishenriqueds01@gmail.com>
2026-02-05 15:03:44 -0300

    feat(backend): estrutura inicial do spring

---
+++ backend/src/main/java/com/es/cinema/tickets/CinemaTicketsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.es.cinema.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketsBackendApplication.class, args);
+	}
+
+}
